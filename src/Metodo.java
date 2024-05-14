13c11a8d3dcddfa6201066bd757d6c5f321892b3
Douglas Grund <douglasgrund@yahoo.com.br>
2024-05-13 21:16:37 -0300

    add new metodo

---
+++ src/Metodo.java
@@ -0,0 +1,16 @@
+public class Metodo {
+
+public static void main(String[] args) {
+  String primeiroNome = "Douglas";
+  String segundoNome = "Grund";
+
+  String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+  System.out.println(nomeCompleto);
+
+}
+
+public static String nomeCompleto (String primeiroNome,String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+}
